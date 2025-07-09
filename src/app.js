2631e6199307962f2f5d2c6e6a90011333a83e2e
Yash <yaswanth.chundru@gmail.com>
2025-07-09 11:16:37 +0530

    Regex and req.query and params

--- src/app.js
+++ src/app.js
@@ -2,27 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({firstname : "Ch", lastname : "Yashwanth"});
+app.get("/ab*c", (req, res) => {
+    res.send("Anything between ab and c can be given")
 });
 
-app.post("/user", (req,res) => {
-    //details will be saved in DB
-    res.send("New User Data has been added successfully!")
+app.get(/a/, (req, res) => {
+    res.send("Anything with a in the string")
 });
 
-app.put("/user", (req,res) => {
-    res.send("Whole Data has been replaced successfully!")
-});
+app.get("/user", (req,res) => {
+    console.log(req.query)
+    res.send("Anything in the path like /user?userid=value can be given that will be given in the above console log")
+})
 
-app.patch("/user", (req,res) => {
-    
-    res.send("Req Data has been updated successfully!")
-});
+app.get("/user/:userid", (req,res) => {
+    console.log(req.params)
+    res.send("predefine the id like you dont need give a query, direclty you can give /user/value that value will be the userid")
+})
 
-app.delete("/user", (req,res) => {
-    res.send("Respectice Data has been deleted successfully!")
-});
+app.get("/user/:userid/:password", (req,res) => {
+    console.log(req.params)
+    res.send("predefine the id like you dont need give a query, direclty you can give /user/value/passvalues that values will be the userid and password")
+})
+
 
 app.listen(7777, ()=>{
     console.log("Successfully listening on port number 7777...")
