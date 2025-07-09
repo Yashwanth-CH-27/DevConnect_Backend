fb6e47a42ec1b4a08c1d2c26e2ed6e3607db6214
Yash <yaswanth.chundru@gmail.com>
2025-07-09 17:31:30 +0530

    Mutiple Route handlers

--- src/app.js
+++ src/app.js
@@ -2,28 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ab*c", (req, res) => {
-    res.send("Anything between ab and c can be given")
-});
-
-app.get(/a/, (req, res) => {
-    res.send("Anything with a in the string")
-});
-
-app.get("/user", (req,res) => {
-    console.log(req.query)
-    res.send("Anything in the path like /user?userid=value can be given that will be given in the above console log")
-})
-
-app.get("/user/:userid", (req,res) => {
-    console.log(req.params)
-    res.send("predefine the id like you dont need give a query, direclty you can give /user/value that value will be the userid")
-})
-
-app.get("/user/:userid/:password", (req,res) => {
-    console.log(req.params)
-    res.send("predefine the id like you dont need give a query, direclty you can give /user/value/passvalues that values will be the userid and password")
-})
+app.use("/user", (req,res,next) => {
+    console.log("Response 1");
+    next();
+},
+(req,res,next) => {
+    console.log("Response 2")
+    next();
+},
+(req,res) => {
+    console.log("Response 3")
+    res.send("3rd Respone")
+},
+)
 
 
 app.listen(7777, ()=>{
