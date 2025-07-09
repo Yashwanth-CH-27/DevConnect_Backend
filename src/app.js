f89046cbc850b2d8f644e8dfaea32cd92dbd54db
Yash <yaswanth.chundru@gmail.com>
2025-07-09 18:36:04 +0530

    Intro for Middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
 const express = require("express");
 
+const {adminAuth} = require("./middlewares")
+
 const app = express();
 
-//app.user("route", rh1,rh2,rh3...) or
-//app.user("route", [rh1,rh2],rh3...) both are same gives you the  same results
+app.use("/admin" ,adminAuth , (req,res) => {
+    res.send("Data sent")
+});
 
-app.use("/user", (req,res,next) => {
-    console.log("Response 1");
-    next();
-},
-(req,res,next) => {
-    console.log("Response 2")
-    next();
-},
-(req,res) => {
-    console.log("Response 3")
-    res.send("3rd Respone")
-},
-)
 
 
 app.listen(7777, ()=>{
