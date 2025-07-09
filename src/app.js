b71203d9411c03561cbabf07c6ffbadd7ace3ef5
Yash <yaswanth.chundru@gmail.com>
2025-07-09 10:45:01 +0530

    Http Crud Intro

--- src/app.js
+++ src/app.js
@@ -2,16 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hi Human!");
+app.get("/user", (req, res) => {
+    res.send({firstname : "Ch", lastname : "Yashwanth"});
 });
 
-app.get("/hello", (req, res) => {
-    res.send("Hello Human!");
+app.post("/user", (req,res) => {
+    //details will be saved in DB
+    res.send("New User Data has been added successfully!")
 });
 
-app.get("/whatsup", (req, res) => {
-    res.send("whatsup");
+app.put("/user", (req,res) => {
+    res.send("Whole Data has been replaced successfully!")
+});
+
+app.patch("/user", (req,res) => {
+    
+    res.send("Req Data has been updated successfully!")
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Respectice Data has been deleted successfully!")
 });
 
 app.listen(7777, ()=>{
