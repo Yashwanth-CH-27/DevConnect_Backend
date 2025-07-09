c6a12d29a8662c784796f910982079c57e7e58a9
Yash <yaswanth.chundru@gmail.com>
2025-07-09 17:33:36 +0530

    mutliplte route handlers formats

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+//app.user("route", rh1,rh2,rh3...) or
+//app.user("route", [rh1,rh2],rh3...) both are same gives you the  same results
+
 app.use("/user", (req,res,next) => {
     console.log("Response 1");
     next();
