85797e8aa05addcc9054059c571067a6220ebed3
Yash <yaswanth.chundru@gmail.com>
2025-07-10 15:29:16 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -5,10 +5,21 @@ const {adminAuth} = require("./middlewares")
 const app = express();
 
 app.use("/admin" ,adminAuth , (req,res) => {
-    res.send("Data sent")
+    try{
+        throw new Error("Wanted Error")
+        res.send("Error sent")
+    }
+    catch(err){
+        res.status(500).send("Something Went wrong!!!")
+    }
 });
-
-
+//the below is also a way of error handling it handles all the errors since it matches all the paths,
+//this is used at the last of the code flow, but always try to do it using try and catch.
+// app.use("/", (err,req,res,next)=>{
+//     if(err){
+//         res.status(500).send("Something Went wrong")
+//     }
+// });
 
 app.listen(7777, ()=>{
     console.log("Successfully listening on port number 7777...")
