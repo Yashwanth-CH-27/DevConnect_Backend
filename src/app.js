98007ad5647f8e5f67c8adfd3f58bea95be69e35
Yash <yaswanth.chundru@gmail.com>
2025-07-10 15:58:56 +0530

    establishing connection to Db using Mongoose

--- src/app.js
+++ src/app.js
@@ -1,26 +1,14 @@
 const express = require("express");
-
-const {adminAuth} = require("./middlewares")
-
+const connectDB = require("./config/database");
 const app = express();
 
-app.use("/admin" ,adminAuth , (req,res) => {
-    try{
-        throw new Error("Wanted Error")
-        res.send("Error sent")
-    }
-    catch(err){
-        res.status(500).send("Something Went wrong!!!")
-    }
-});
-//the below is also a way of error handling it handles all the errors since it matches all the paths,
-//this is used at the last of the code flow, but always try to do it using try and catch.
-// app.use("/", (err,req,res,next)=>{
-//     if(err){
-//         res.status(500).send("Something Went wrong")
-//     }
-// });
-
-app.listen(7777, ()=>{
-    console.log("Successfully listening on port number 7777...")
-});
+connectDB()
+  .then(() => {
+    console.log("DB connected Successfully");
+    app.listen(7777, () => {
+      console.log("Successfully listening on port number 7777...");
+    });
+  })
+  .catch((err) => {
+    console.log("DB is not connected!!");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://Yaswanth:Yash%40270821@namastenode.dxuqtrk.mongodb.net/")
+};
+
+module.exports = connectDB;
