c48f2b3b11933145894d088f5d1e0d3466aa9297
Yash <yaswanth.chundru@gmail.com>
2025-07-10 17:49:34 +0530

    Using Mangoose To Create Schema and Model and Adding data to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signUp", async (req,res) => {
+    const user = new User({
+        firstName: "Chundru",
+        lastName: "Yaswanth Sai Kiran",
+        emailId: "yash@gmail.com",
+        password: "yash@123",
+    })
+    await user.save();
+    res.send("New User Data added Successfully!!")
+})
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://Yaswanth:Yash%40270821@namastenode.dxuqtrk.mongodb.net/")
+    await mongoose.connect("mongodb+srv://Yaswanth:Yash%40270821@namastenode.dxuqtrk.mongodb.net/devTinder")
 };
 
 module.exports = connectDB;
