aadfd0c1f28e5d60593c681b758a570fa1b92943
Yash <yaswanth.chundru@gmail.com>
2025-07-15 11:50:46 +0530

    edit password API

--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const profileRouter = express.Router();
 const userAuth = require("../middleware/userAuth");
 const updateValidator = require("../utils/updateValidator")
+const bcrypt = require("bcrypt");
 
 //profile view API
 profileRouter.get("/profile/view", userAuth, async (req,res) => {
@@ -28,5 +29,24 @@ profileRouter.patch("/profile/edit", userAuth, async (req,res) => {
     res.status(404).send("Something went wrong!"+ err.message)
     } 
 });
+
+//profile edit password API
+profileRouter.patch("/profile/edit/password", userAuth, async (req,res) => {
+    try{
+        const {currentPassword} = req.body;
+        const user = req.user;
+        const isPasswordValid = await bcrypt.compare(currentPassword,user.password);
+        if(!isPasswordValid){
+            throw new Error("Password is not matching!");
+        }
+        const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+        user.password = hashedPassword
+        await user.save();
+        res.send("Updation successfull!!")
+      }
+      catch(err){
+        res.status(404).send("Error: "+ err.message);
+      }
+})
 
 module.exports = profileRouter;
